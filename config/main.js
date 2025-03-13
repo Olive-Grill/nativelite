129355f433d0a40df317a48e062505d3374a7f51
Olive-Grill <oligoollie@gmail.com>
2025-03-13 15:06:09 -0400

    Update main.js

--- config/main.js
+++ config/main.js
@@ -25,22 +25,43 @@ function displayFilteredGames(filteredGames) {
     const gameDiv = document.createElement("div");
     gameDiv.classList.add("game");
 
-    const gameImage = document.createElement("img");
-    gameImage.src = `${serverUrl1}/${game.url}/${game.image}`;
-    gameImage.alt = game.name;
-    gameImage.onclick = () => {
-      window.location.href = `play.html?gameurl=${game.url}/`;
-    };
+    // Create image container
+    const imageContainer = document.createElement("div");
+    imageContainer.classList.add("image-container");
 
+    // External Image
+    const gameImage1 = document.createElement("img");
+    gameImage1.src = `${serverUrl1}/${game.url}/${game.image}`;
+    gameImage1.alt = `${game.name} (External)`;
+    gameImage1.classList.add("game-image");
+
+    // Local Image
+    const gameImage2 = document.createElement("img");
+    gameImage2.src = `config/${game.image}`;
+    gameImage2.alt = `${game.name} (Local)`;
+    gameImage2.classList.add("game-image");
+
+    // Clicking any image should load the game
+    [gameImage1, gameImage2].forEach((img) => {
+      img.onclick = () => {
+        window.location.href = `play.html?gameurl=${game.url}`;
+      };
+    });
+
+    // Append both images
+    imageContainer.appendChild(gameImage1);
+    imageContainer.appendChild(gameImage2);
+
+    // Game name
     const gameName = document.createElement("p");
     gameName.textContent = game.name;
 
-    gameDiv.appendChild(gameImage);
+    // Add everything to the game div
+    gameDiv.appendChild(imageContainer);
     gameDiv.appendChild(gameName);
     gamesContainer.appendChild(gameDiv);
   });
 }
-
 
 function handleSearchInput() {
   const searchInputValue = document
