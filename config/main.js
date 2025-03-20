72d37eb69bec17fbea0ece72f720195fb9ea198e
Olive-Grill <oligoollie@gmail.com>
2025-03-20 13:02:47 -0400

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
 var sitename = "Goggle Games"; // Change this to change the name of your website.
-var subtext = "Port Washington's Number One Games Website -- Updated 3/18/25 (V1)"; // set the subtext
+var subtext = "Port Washington's Number One Games Website (V1.5)"; // set the subtext
 
 // more settings in main.css
 
